c3a3b78d7324426cef3ee3b58bf7cb7f1d624fdf
Katrin Tirok <katrintirok@gmail.com>
2019-01-09 22:59:03 +0200

    clean up some code

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -56,39 +56,6 @@ pre_os$os <- factor(pre_os$os, levels = c('Windows', 'Mac OS', 'Linux', 'unknown
 pre_dom <- read_csv('../data/20181002-pre-professional_profile-no-open.csv', 
                     na = '<NA>')
 
-# domains_dc <- dom_splt(pre_dom$domain[pre_dom$workshop_type == 'dc'])
-# domains_swc <- dom_splt(pre_dom$domain[pre_dom$workshop_type == 'swc'])
-# # make a tibble with domains
-# domains <- rbind(tibble(domain = domains_dc, workshop_type = 'dc'),
-#                  tibble(domain = domains_swc, workshop_type = 'swc'))
-# 
-# # combine some similar domains from main list
-# domains$domain[domains$domain == 'Medicine'] <- 'Biomedical or Health Sciences'
-# domains$domain[domains$domain == 'Biomedical or Health Sciences'] <- 'Medical or Health Sciences'
-# domains$domain[domains$domain == 'Earth Sciences'] <- 'Planetary Sciences (Geology, Climatology, Oceanography, etc.)'
-# domains$domain[domains$domain == 'NA'] <- 'Other'
-# 
-# # make dataframe with counts/percentages
-# pre_dom_cnt <- pre_cnt(x = domains, groupvar = c('domain','workshop_type'))
-# 
-# pre_dom_cnt <- domains %>%
-#   dplyr::group_by(domain, workshop_type) %>%
-#   dplyr::count() %>%
-#   dplyr::ungroup() %>%
-#   # percent should be dependent on number of respondendts!
-#   dplyr::group_by(workshop_type) %>%
-#   dplyr::mutate(n_percent = round(100 * n/sum(n))) %>%
-#   dplyr::ungroup()
-# 
-# # prepare data for plotting
-# # combine rare domains (<5 percent) into group Other
-# pre_dom_pl <- pre_dom_cnt %>%
-#   dplyr::mutate(domain = ifelse(n_percent >= 5, domain, 'Other')) %>%
-#   dplyr::group_by(domain, workshop_type) %>%
-#   dplyr::summarise(n_percent_s = sum(n_percent), n_s = sum(n)) %>%
-#   dplyr::ungroup() %>%
-#   arrange(desc(n_s))
-
 # ---- domains ----
 domains <- sep_str(pre_dom$domain)
 # combine some similar domains from main list
@@ -162,8 +129,7 @@ ui <- shinyUI(pageWithSidebar(
     textOutput(outputId = 'question1'),
     plotOutput(outputId = 'osPlot'),
     textOutput(outputId = 'question2'),
-    plotOutput(outputId = 'domPlotbar'),
-    plotOutput(outputId = 'domPlotcircle'),
+    plotOutput(outputId = 'domPlot'),
     textOutput(outputId = 'question3'),
     plotOutput(outputId = 'occPlot')
   )
@@ -194,38 +160,16 @@ server <- shinyServer(function(input, output){
     switch(input$variable,
            "Data Carpentry" = subset(pre_dom, workshop_type == 'dc'),
            "Software Carpentry" = subset(pre_dom, workshop_type == 'swc')
-    ) %>%
-      pre_cnt(groupvar = 'domain')
+    )
   })
   
   occInput <- reactive({
     switch(input$variable,
            "Data Carpentry" = subset(pre_occ_pl, workshop_type == 'dc'),
            "Software Carpentry" = subset(pre_occ_pl, workshop_type == 'swc')
-    ) %>%
-      sep_str(domain) %>%
-      mutate(domains[domains == 'Medicine'] <- 'Biomedical or Health Sciences',
-             domains[domains == 'Biomedical or Health Sciences'] <- 'Medical or Health Sciences',
-             domains[domains == 'Earth Sciences'] <- 'Planetary Sciences (Geology, Climatology, Oceanography, etc.)') %>%
-      tibble()
+    )
   })
   
-  
-  domains <- sep_str(pre_dom$domain)
-  # combine some similar domains from main list
-  domains[domains == 'Medicine'] <- 'Biomedical or Health Sciences'
-  domains[domains == 'Biomedical or Health Sciences'] <- 'Medical or Health Sciences'
-  domains[domains == 'Earth Sciences'] <- 'Planetary Sciences (Geology, Climatology, Oceanography, etc.)'
-  #domains[domains == 'NA'] <- 'Other'
-  # make a tibble with domains
-  domains <- dplyr::tibble(domain = domains)
-  
-  # make dataframe with counts/percentages
-  pre_dom_cnt <- pre_cnt(domains, 'domain')
-  
-  # prepare data for plotting
-  # combine rare domains (<5 percent) into group Other
-  pre_dom_pl <- pre_pl(pre_dom_cnt, column_comb = 'domain', percent_comb = 2)
   
   # return the formula text for printing as a caption
   output$caption <- renderText({
@@ -242,14 +186,14 @@ server <- shinyServer(function(input, output){
   
   output$question2 <- renderText({
     paste('Participants in your workshop mainly have a background in ',
-          domInput()$domain[1], ', ', domInput()$domain[2], ', and ', domInput()$domain[3], '.',
+          pre_dom_pl$domain[1], ', ', pre_dom_pl$domain[2], ', and ', pre_dom_pl$domain[3], '.',
           sep = '')
   })
   
-  output$question3 <- renderText({
-    paste('Most participants in your workshop will be ',
-          occInput()$occupation[1], occInput()$occupation[2], occInput()$occupation[3])
-  })
+#  output$question3 <- renderText({
+#    paste('Most participants in your workshop will be ',
+#          occInput()$occupation[1], occInput()$occupation[2], occInput()$occupation[3])
+#  })
 
   
   # ---- generate plots of the requested variable ----
@@ -265,51 +209,50 @@ server <- shinyServer(function(input, output){
   }) # end plot 1
   
   # start plot 2 - barplot
-  output$domPlotbar <- renderPlot(width = 500, height = 250, {
-    validate(need(input$plottype == "barplot", message=FALSE))
-    ggplot(data = pre_dom_pl) +
-      geom_bar(aes(x = reorder(domain,n_s), y = n_percent_s), 
-               stat='identity', fill = 'grey') + 
-      geom_text(aes(x = domain, y = n_percent_s/2, label = n_s), 
-                stat = 'identity', size = 4) + 
-      coord_flip() +
-      # remove padding on percentage axis, set limits
-     # scale_y_continuous(expand = c(0,0), 
-    #                     limits = c(0, ifelse(signif(max(domInput()$n_percent_s),1)>=max(domInput()$n_percent_s),
-    #                                 signif(max(domInput()$n_percent_s),1), 
-    #                                 signif(max(domInput()$n_percent_s),1)+10))) +
-      labs(y = '% of participants', x = '') +
-      theme_classic() +
-      theme(axis.ticks.y = element_blank(),
-            axis.line.y = element_blank(),
-            axis.text.y=element_text(size = 10))
-  }) # end plot 2 - barplot
+  output$domPlot <- renderPlot(width = 500, height = 250, {
+    #validate(need(input$plottype == "barplot", message=FALSE))
+    if(input$plottype == 'barplot'){ # barplot
+      ggplot(data = pre_dom_pl) +
+        geom_bar(aes(x = reorder(domain,n_s), y = n_percent_s), 
+                 stat='identity', fill = 'grey') + 
+        geom_text(aes(x = domain, y = n_percent_s/2, label = n_s), 
+                  stat = 'identity', size = 4) + 
+        coord_flip() +
+        # remove padding on percentage axis, set limits
+        scale_y_continuous(expand = c(0,0), 
+                            limits = c(0, ifelse(signif(max(pre_dom_pl$n_percent_s),1)>=max(pre_dom_pl$n_percent_s),
+                                        signif(max(pre_dom_pl$n_percent_s),1), 
+                                        signif(max(pre_dom_pl$n_percent_s),1)+10))) +
+        labs(y = '% of participants', x = '') +
+        theme_classic() +
+        theme(axis.ticks.y = element_blank(),
+              axis.line.y = element_blank(),
+              axis.text.y=element_text(size = 10))
+    } else { # circleplot
+      # Generate the layout. This function return a dataframe with one line per bubble. 
+      # It gives its center (x and y) and its radius, proportional of the value
+      packing <- circleProgressiveLayout(pre_dom_pl$n_s, sizetype='area')
+      # We can add these packing information to the initial data frame
+      data = cbind(pre_dom_pl, packing)
+      # Check that radius is proportional to value. We don't want a linear relationship, since it is the AREA that must be proportionnal to the value
+      plot(data$radius, data$n_s)
+      # The next step is to go from one center + a radius to the coordinates of a circle that
+      # is drawn by a multitude of straight lines.
+      dat.gg <- circleLayoutVertices(packing, npoints=50)
+      # Make the plot
+      ggplot() + 
+        # Make the bubbles
+        geom_polygon(data = dat.gg, aes(x, y, group = id, fill=as.factor(id)), colour = "black", alpha = 0.6) +
+        # Add text in the center of each bubble + control its size
+        geom_text(data = data, aes(x, y, label = domain)) +   #size=n_s, 
+        scale_size_continuous(range = c(1,4)) +
+        # General theme:
+        theme_void() + 
+        theme(legend.position="none") +
+        coord_equal()
+    } #end if else statement
+  }) # end plot 2
   
-  # start plot 2 - circleplot
-  output$domPlotcircle <- renderPlot(width = 500, height = 250, {
-    validate(need(input$plottype == "circleplot", message=FALSE))
-    # Generate the layout. This function return a dataframe with one line per bubble. 
-    # It gives its center (x and y) and its radius, proportional of the value
-    packing <- circleProgressiveLayout(pre_dom_pl$n_s, sizetype='area')
-    # We can add these packing information to the initial data frame
-    data = cbind(pre_dom_pl, packing)
-    # Check that radius is proportional to value. We don't want a linear relationship, since it is the AREA that must be proportionnal to the value
-    plot(data$radius, data$n_s)
-    # The next step is to go from one center + a radius to the coordinates of a circle that
-    # is drawn by a multitude of straight lines.
-    dat.gg <- circleLayoutVertices(packing, npoints=50)
-    # Make the plot
-    ggplot() + 
-      # Make the bubbles
-      geom_polygon(data = dat.gg, aes(x, y, group = id, fill=as.factor(id)), colour = "black", alpha = 0.6) +
-      # Add text in the center of each bubble + control its size
-      geom_text(data = data, aes(x, y, label = domain)) +   #size=n_s, 
-      scale_size_continuous(range = c(1,4)) +
-      # General theme:
-      theme_void() + 
-      theme(legend.position="none") +
-      coord_equal()
-  }) # end plot2 - circleplot
-})
+}) # end server
 
 shinyApp(ui = ui, server = server)
